b35e85dbc4e4057b587a67a70f5a5d31274a4e16
Diana-Kaba <kabadiana327@gmail.com>
2023-11-04 14:27:01 +0200

    initial commit

--- index.php
+++ index.php
@@ -8,16 +8,6 @@
 <body>
     <h1>Кінотеатр</h1>
     <form action="db-actions.php" method="POST" name="search">
-    <!-- <label for="request">Оберіть за чим запит: </label>
-    <select name="request">
-            <option value="director">Режисер</option>
-            <option value="year">Дата випуску</option>
-            <option value="name">Назва</option>
-            <option value="genre">Жанр</option>
-            <option value="rating">Рейтинг</option>
-            <option value="studio">Кіностудія</option>
-            <option value="sessions">Сеанси</option>
-    </select> -->
     <label for="inputData">Введіть запит: </label>
     <input type="text" name="inputData" required>
     <input type="submit" value="Зберегти" name="sendingSearch">
