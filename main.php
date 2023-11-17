18c5f077d8f9aca2ee11cd76369c30f4ab341cd7
Diana <kabadiana327@gmail.com>
2023-11-17 17:24:12 +0200

    Added contact section

--- main.php
+++ main.php
@@ -169,3 +169,39 @@ if (isset($_POST['sendingSearch'])) {
 </div>
 
 <!-- Sort & search films end  -->
+
+<!-- Contact start -->
+
+<div class="contact-page section" id="contacts">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 align-self-center">
+          <div class="left-text">
+            <div class="section-heading">
+              <h6>Контакти</h6>
+              <h2>Замовьте білет!</h2>
+            </div>
+            <p>Сьогодні кінотеатр є повноцінним незалежним кінотеатром, який демонструє фільми 362 дні на рік. Програма варіюється від старовинних класичних фільмів до сучасних блокбастерів, які часто збирають великі касові черги.</p>
+            <ul>
+              <li><span>Address</span> High St, Berkhamsted HP4 2FG, Велика Британія</li>
+              <li><span>Phone</span> +441442877759</li>
+              <li><span>Email</span> rex@contact.com</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="right-content">
+            <div class="row">
+              <div class="col-lg-12">
+                <div id="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2469.601262814349!2d-0.5630307233648554!3d51.75861447187152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48764353f5719d91%3A0xf88df297ac79b34!2sThe%20Rex%20Cinema!5e0!3m2!1suk!2sua!4v1700234287164!5m2!1suk!2sua" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<!-- Contact end -->
